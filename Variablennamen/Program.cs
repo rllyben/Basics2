b5a3c8d3de72b908b46cdcea5cf6191a9a3d5b73
rllyben <b.yumitoya@gmail.com>
2024-09-16 07:43:00 +0200

    Variablennamen

---
+++ Variablennamen/Program.cs
@@ -0,0 +1,32 @@
+namespace Variablennamen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 10;
+            // erstes zeichen darf keine Zahl sein!
+            // int 1sdf = 10;
+
+            // private Variablen
+            int _privtest = 10;
+
+            // Pascal case
+            int HelloWorld = 10;
+
+            // Camel case
+            int helloworld = 10;
+
+            // keine Spaces im Namen
+            // int Hello World = 10;
+
+            // verwendet ein _ stattdessen
+            int Hello_World = 10;
+
+            // Pascal case
+            Console.WriteLine();
+        }
+
+    }
+
+}
